e6dd0cbc65926b3e2d7e288ab61a4b8b009e92e5
liliya <liliya.fedyurina@gmail.com>
2021-02-23 11:04:34 -0800

    Selection Sort added
    Selection Sort

--- src/exponentialSearch/expSearch.java
+++ src/exponentialSearch/expSearch.java
@@ -6,7 +6,7 @@ public class expSearch {
 	public static  void main(String[] args){
 		 int[] arr = {6,12,18,24,32};
 		 int length= arr.length;
-		 int value = 18;
+		 int value = 12;
 		 int outcome = exponentialSearch(arr,length,value);
 		 
 		 if(outcome>0) {
@@ -16,7 +16,8 @@ public class expSearch {
 }
 	 public static int exponentialSearch(int[] arr ,int length, int value ){
 		 if(arr[0]==value){
-			 return 0;
+			
+			 System.out.println("Value is 0");
 		 }
 		 int i=1;
 		 while(i<length && arr[i]<=value){
